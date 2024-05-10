b2001f1fb454476a468dcffba5a8efacaa2a15cd
Imisioluwa3 <oluwajobaimisioluwa@gmail.com>
2024-05-10 16:15:14 +0100

    Py

--- generating_csv.py
+++ generating_csv.py
@@ -5,7 +5,16 @@ hosts = [["workstation.local","192.168.25.46"],["webserver.cloud","10.2.5.6"]]
 def generate_csv(csv_file_path):
     with open(csv_file_path,'w', newline='') as hosts_csv:
         writer = csv.writer(hosts_csv)
+        writer.writerow(["name", "address"])
         writer.writerows(hosts)
         
         
 generate_csv("hosts.csv")
+
+def reading_csv(csv_file):
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            print("{}'s IP Address is {}.".format(row["name"], row["address"]))
+            
+reading_csv("hosts.csv")
